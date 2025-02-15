68bc85ef53c568c3b0f73f5f0b03a0f947a5af15
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-02-15 16:10:34 +0700

    arrow function ()=>

--- dist/app.js
+++ dist/app.js
@@ -15,16 +15,20 @@
 // }
 // console.log(getAddress());
 // function รับและ return
-function checkNumber(num) {
-    if (num % 2 === 0) {
-        return "เลขคู่";
-    }
-    else {
-        return "เลขคี่";
-    }
-}
-function total(a, b) {
-    return a + b;
-}
-console.log(total(100, 200));
-console.log(checkNumber(11));
+// function checkNumber(num:number):string{
+//     if(num % 2 === 0){
+//         return "เลขคู่"
+//     }else{
+//         return "เลขคี่"
+//     }
+// }
+// function total(a:number,b:number){
+//     return a+b
+// }
+// console.log(total(100,200))
+// console.log(checkNumber(11))
+// arrow function
+// const total=(a:number,b:number):number=>{
+//     return a+b;
+// }
+// console.log( `total = ${total(100,200)}`)
